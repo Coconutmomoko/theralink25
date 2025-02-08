837ca9aa4c634dbddb7ebcbc262dab6560b6d444
Ej Sobrepeña <sobrepenaej@yahoo.com>
2025-02-08 23:53:15 +0200

    Add HTTPS support to server.js with conditional server creation

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const socketIo = require("socket.io");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-const server = http.createServer(app);
+let server;
+
+if (process.env.USE_HTTPS) {
+  const options = {
+    key: fs.readFileSync("certificates/cert.key"),
+    cert: fs.readFileSync("certificates/cert.crt"),
+  };
+  server = https.createServer(options, app);
+  console.log("Using HTTPS");
+} else {
+  server = http.createServer(app);
+  console.log("Using HTTP");
+}
+
 const io = socketIo(server, { secure: true });
 
 app.use(express.static(path.join(__dirname, "../frontend")));
