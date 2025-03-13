d6d31c3b1bfc029e157213647b37b10aac4e6dcd
subriais <subria.islam@outlook.com>
2025-03-13 22:53:55 +0200

    improved code for screen shearing

--- frontend/main.js
+++ frontend/main.js
@@ -90,6 +90,7 @@ async function startCall() {
   peerConnection.ontrack = (event) => {
     remoteStream.addTrack(event.track); // Add track to remoteStream
     remoteVideo.srcObject = remoteStream;
+    remoteVideo.style.transform = "";  // screen sharing
   };
 
   const offer = await peerConnection.createOffer();
